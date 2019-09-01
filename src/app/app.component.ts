9b1dd3ed9053916a46ef4ff191a07270e390bb7e
LASLEDJ <b.lasledj@gmail.com>
2019-09-01 13:30:56 +0200

    add nested form-group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'nested-form-group';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+
+  private usersData = [
+    {
+      name: 'Bob',
+      age: 20,
+    },
+  ];
+
+  get users(): FormArray {
+    return this.myForm.get('users') as FormArray;
+  }
+
+  constructor(private readonly formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      users: this.formBuilder.array(this.usersData.map(user => this.formBuilder.group(user))),
+    });
+  }
+
+  addUser() {
+    this.users.push(this.formBuilder.group({
+      name: '',
+      age: null,
+    }));
+  }
+
+  onSubmit() {
+    console.log(this.users.value);
+    console.log(this.myForm.value);
+  }
 }
